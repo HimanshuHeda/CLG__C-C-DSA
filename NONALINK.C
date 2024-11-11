05d1f67a6883515031c7a1453f137a3ca87ec3c9
HimanshuHeda <harsh967896@gmail.com>
2024-11-11 09:48:23 +0530

    first commit

---
+++ NONALINK.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+#include<conio.h>
+struct node
+{
+int data;
+struct node*next;
+struct node*prevs;
+};
+struct node*start
+int num;
+char choice;
+int val;
+struct node*cur;
+void created (struct node);
+void display(struct node);
+
+
